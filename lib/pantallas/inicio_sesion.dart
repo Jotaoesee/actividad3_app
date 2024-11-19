34aeb7733ad1bf73ceed35f76a2d2ff9f922f092
Jotaoesee <jadiazm1995@gmail.com>
2024-11-19 20:32:26 +0100

    pruebo commit y creo pantalla para el inicio de sesion

---
+++ lib/pantallas/inicio_sesion.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InicioSesion extends StatelessWidget {
+  const InicioSesion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    MaterialApp app =  const MaterialApp(title: "Mi primera App",);
+    return app;
+  }
+}
