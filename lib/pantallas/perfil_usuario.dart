e00a9ae71fa73fecd42ef808fd8767a857b79589
Jotaoesee <jadiazm1995@gmail.com>
2024-11-22 20:41:02 +0100

    Añado la opcion de que pueda tomar una foto o selecionar una foto en su perfi, he puesto una condicion de que si es en web solo se pueda seleccionar fotos y si es para movil se pueda tanto seleccionar como tomar foto

--- lib/pantallas/perfil_usuario.dart
+++ lib/pantallas/perfil_usuario.dart
@@ -1,6 +1,10 @@
 import 'package:actividad3_app/pantallas/splash.dart';
 import 'package:actividad3_app/personalizable/boton/boton_personalizado.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_picker_web/image_picker_web.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class PerfilUsuario extends StatefulWidget {
   const PerfilUsuario({Key? key}) : super(key: key);
@@ -14,17 +18,16 @@ class _PerfilUsuarioState extends State<PerfilUsuario> {
   String _apellido = "Apellido de Usuario";
   String _telefono = "+123 456 789";
   String _ciudad = "Ciudad, País";
-  String _edad = "30";
   String _fechaNacimiento = "01/01/1990";
+
+  File? _imagenPerfil; // Variable para almacenar la imagen de perfil
 
   final TextEditingController _nombreController = TextEditingController();
   final TextEditingController _apellidoController = TextEditingController();
   final TextEditingController _telefonoController = TextEditingController();
   final TextEditingController _ciudadController = TextEditingController();
-  final TextEditingController _edadController = TextEditingController();
   final TextEditingController _fechaNacimientoController = TextEditingController();
 
-  // Esquema de colores similar al de la clase Ajuste
   final Map<String, List<Color>> esquemasDeColor = {
     'Azul': [Color(0xFF0D47A1), Color(0xFF1F77D3), Color(0xFF4AA3F3)],
     'Verde': [Color(0xFF1B5E20), Color(0xFF4CAF50), Color(0xFF81C784)],
@@ -49,92 +52,102 @@ class _PerfilUsuarioState extends State<PerfilUsuario> {
           title: const Text("Perfil del Usuario"),
           backgroundColor: fondoColor,
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: esquemasDeColor[_esquemaColor]!,
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: esquemasDeColor[_esquemaColor]!,
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20),
-                // Foto de perfil
-                const CircleAvatar(
-                  radius: 70,
-                  backgroundImage: NetworkImage(
-                    'https://via.placeholder.com/150', // URL de imagen de perfil
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  // Foto de perfil
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: _imagenPerfil == null
+                        ? const NetworkImage('https://via.placeholder.com/150')
+                        : FileImage(_imagenPerfil!) as ImageProvider,
                   ),
-                ),
-                const SizedBox(height: 20),
-                // Nombre del usuario
-                Text(
-                  _nombre,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                  IconButton(
+                    icon: const Icon(Icons.camera_alt),
+                    onPressed: () {
+                      _mostrarOpcionesImagen();
+                    },
                   ),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  "usuario@correo.com", // El correo es estático por ahora
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
+                  const SizedBox(height: 20),
+                  // Nombre del usuario
+                  Text(
+                    _nombre,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 30),
-                // Información adicional en Card con bordes redondeados
-                _crearCard("Nombre", _nombre, _nombreController, (nuevoValor) {
-                  setState(() {
-                    _nombre = nuevoValor;
-                  });
-                }),
-                _crearCard("Apellido", _apellido, _apellidoController, (nuevoValor) {
-                  setState(() {
-                    _apellido = nuevoValor;
-                  });
-                }),
-                _crearCard("Fecha de nacimiento", _fechaNacimiento, _fechaNacimientoController, (nuevoValor) {
-                  setState(() {
-                    _fechaNacimiento = nuevoValor;
-                  });
-                }),
-                _crearCard("Teléfono", _telefono, _telefonoController, (nuevoValor) {
-                  setState(() {
-                    _telefono = nuevoValor;
-                  });
-                }),
-                _crearCard("Ciudad", _ciudad, _ciudadController, (nuevoValor) {
-                  setState(() {
-                    _ciudad = nuevoValor;
-                  });
-                }),
+                  const SizedBox(height: 8),
+                  const Text(
+                    "usuario@correo.com",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  // Información adicional en Card con bordes redondeados
+                  _crearCard("Nombre", _nombre, _nombreController, (nuevoValor) {
+                    setState(() {
+                      _nombre = nuevoValor;
+                    });
+                  }),
+                  _crearCard("Apellido", _apellido, _apellidoController, (nuevoValor) {
+                    setState(() {
+                      _apellido = nuevoValor;
+                    });
+                  }),
+                  _crearCard("Fecha de nacimiento", _fechaNacimiento, _fechaNacimientoController, (nuevoValor) {
+                    setState(() {
+                      _fechaNacimiento = nuevoValor;
+                    });
+                  }),
+                  _crearCard("Teléfono", _telefono, _telefonoController, (nuevoValor) {
+                    setState(() {
+                      _telefono = nuevoValor;
+                    });
+                  }),
+                  _crearCard("Ciudad", _ciudad, _ciudadController, (nuevoValor) {
+                    setState(() {
+                      _ciudad = nuevoValor;
+                    });
+                  }),
 
-                const SizedBox(height: 40),
-                // Botón de cerrar sesión
-                BotonPersonalizado(
-                  texto: "Cerrar sesión",
-                  icono: Icons.logout,
-                  alPresionar: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) =>  Splash())
-                    );
-                  },
-                ),
-              ],
+                  const SizedBox(height: 5),  // Ajusta este espacio según lo necesites
+                  // Botón de cerrar sesión
+                  BotonPersonalizado(
+                    texto: "Cerrar sesión",
+                    icono: Icons.logout,
+                    alPresionar: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Splash())
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+
       ),
     );
   }
+
 
   // Método para crear las cards de cada campo editable
   Widget _crearCard(String titulo, String campo, TextEditingController controlador, Function(String) onGuardar) {
@@ -158,7 +171,12 @@ class _PerfilUsuarioState extends State<PerfilUsuario> {
   }
 
   // Método para mostrar el cuadro de diálogo de edición
-  void _mostrarDialogoEdicion(String titulo, String campo, TextEditingController controlador, VoidCallback onGuardar) {
+  void _mostrarDialogoEdicion(
+      String titulo,
+      String campo,
+      TextEditingController controlador,
+      VoidCallback onGuardar) {
+
     controlador.text = campo;
 
     showDialog(
@@ -188,5 +206,82 @@ class _PerfilUsuarioState extends State<PerfilUsuario> {
         );
       },
     );
+  }
+
+  // Método para mostrar las opciones de imagen
+  void _mostrarOpcionesImagen() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Selecciona una opción"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.image),
+                title: const Text("Seleccionar de la galería"),
+                onTap: () {
+                  Navigator.pop(context);
+                  _seleccionarImagen(); // Llamar a seleccionar imagen
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.camera),
+                title: const Text("Tomar foto"),
+                onTap: () {
+                  Navigator.pop(context);
+                  _tomarFoto(); // Llamar a tomar foto
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Método para seleccionar imagen de la galería
+  Future<void> _seleccionarImagen() async {
+    if (kIsWeb) {
+      // Para web, usar ImagePickerWeb para seleccionar imagen de la galería
+      final pickedFile = await ImagePickerWeb.getImageAsFile();
+      if (pickedFile != null) {
+        setState(() {
+          _imagenPerfil = pickedFile as File?; // Asigna la imagen seleccionada
+        });
+      }
+    } else {
+      // Para móvil, usar ImagePicker para seleccionar imagen de la galería
+      final ImagePicker _picker = ImagePicker();
+      final XFile? imagen = await _picker.pickImage(source: ImageSource.gallery);
+      if (imagen != null) {
+        setState(() {
+          _imagenPerfil = File(imagen.path); // Asigna la imagen seleccionada
+        });
+      }
+    }
+  }
+
+  // Método para tomar foto con la cámara
+  Future<void> _tomarFoto() async {
+    if (kIsWeb) {
+      // Para web, usar ImagePickerWeb para tomar foto
+      final pickedFile = await ImagePickerWeb.getImageAsFile();
+      if (pickedFile != null) {
+        setState(() {
+          _imagenPerfil = pickedFile as File?; // Asigna la foto tomada
+        });
+      }
+    } else {
+      // Para móvil, usar ImagePicker para tomar foto
+      final ImagePicker _picker = ImagePicker();
+      final XFile? imagen = await _picker.pickImage(source: ImageSource.camera);
+      if (imagen != null) {
+        setState(() {
+          _imagenPerfil = File(imagen.path); // Asigna la foto tomada
+        });
+      }
+    }
   }
 }
